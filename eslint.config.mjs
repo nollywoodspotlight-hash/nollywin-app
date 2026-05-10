17d85e4bcbf7d93466e10ba7ca7cee9cb29c9d91
Rebeccah Mokuolu <rebeccah.monyei@gmail.com>
2026-05-09 23:16:15 -0500

    Final cleanup: Removed farcaster-provider and fixed ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript";
+import "eslint-config-next/typescript.js";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
